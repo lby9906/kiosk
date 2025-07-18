86d1b58ca43d3c70f757cbf10ea2875a6127153c
lby9906 <lby9906@naver.com>
2025-07-18 15:42:36 +0900

    fix: lv2 try-catch문 및 개선된 switch문 코드 추가

--- src/main/java/org/example/lv2/Main2.java
+++ src/main/java/org/example/lv2/Main2.java
@@ -1,6 +1,7 @@
 package org.example.lv2;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -22,23 +23,28 @@ public class Main2 {
         }
 
         System.out.println();
-        System.out.print("번호를 입력하세요: ");
-        int num = in.nextInt();
 
-        while(true) {
-            switch (num) {
-                case 1: System.out.println("선택한 메뉴: ShackBurger | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+        while (true) {
+            System.out.print("메뉴 번호를 입력하세요: ");
+            try {
+                int menuSelect = in.nextInt();
+
+                if (menuSelect == 0) {
+                    System.out.println("프로그램을 종료합니다.");
                     break;
-                case 2: System.out.println("선택한 메뉴: SmokeShack | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-                    break;
-                case 3: System.out.println("선택한 메뉴: Cheeseburger | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-                    break;
-                case 4: System.out.println("선택한 메뉴: Hamburger | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
-                    break;
-                case 0: System.out.println("프로그램을 종료합니다.");
-                    break;
+                } else if (menuSelect < 0 || menuSelect > 4) {
+                    System.out.println("주어진 번호만 입력해주세요.");
+                    continue;
+                }
+                switch (menuSelect) {
+                    case 1 -> System.out.println("선택한 메뉴 : ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+                    case 2 -> System.out.println("선택한 메뉴 : SmokeShack    | W 6.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+                    case 3 -> System.out.println("선택한 메뉴 : Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+                    case 4 -> System.out.println("선택한 메뉴 : Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+                }
+            }catch (InputMismatchException e) {
+                throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
             }
-            break;
         }
     }
 }
