26f7c86e146e477c4fb740f57c24fc04ae1bc4e4
lby9906 <lby9906@naver.com>
2025-07-23 16:03:40 +0900

    fix: challenge lv2 DiscountRate.java 할인율 통합 메서드 구현 및 개선

--- src/main/java/org/example/challenge/lv2/DiscountRate.java
+++ src/main/java/org/example/challenge/lv2/DiscountRate.java
@@ -4,40 +4,22 @@ public class DiscountRate {
 
     public void printDiscountRate() {
         System.out.println("할인 정보를 입력해주세요.");
-        System.out.println(
-                 "1. 국가유공자 : 10% \n" +
-                 "2. 군인     :  5% \n" +
-                 "3. 학생     :  3% \n" +
-                 "4. 일반     :  0%");
+
+        String printDiscount = """
+                1. 국가유공자 : 10%
+                2. 군인     :  5%
+                3. 학생     :  3%
+                4. 일반     :  0%
+                """;
+        System.out.println(printDiscount);
     }
 
-    public double discountStudent(DiscountRateType discountRateType, double totalPrice) {
-        if (discountRateType == DiscountRateType.STUDENT) {
-            double discount = totalPrice * 0.03;
-            totalPrice = totalPrice - discount;
-        }
-        return totalPrice;
-    }
-
-    public double discountMilitary(DiscountRateType discountRateType, double totalPrice) {
-        if (discountRateType == DiscountRateType.MILITARY) {
-            double discount = totalPrice * 0.05;
-            totalPrice = totalPrice - discount;
-        }
-        return totalPrice;
-    }
-
-    public double discountNationlMerit(DiscountRateType discountRateType, double totalPrice) {
-        if (discountRateType == DiscountRateType.NATIONL_MERIT) {
-            double discount = totalPrice * 0.1;
-            totalPrice = totalPrice - discount;
-        }
-        return totalPrice;
-    }
-
-    public double discountOrdinary(DiscountRateType discountRateType, double totalPrice) {
-        if (discountRateType == DiscountRateType.ORDINARY) {
-            totalPrice = totalPrice - 0;
+    public double discount(DiscountRateType discountRateType, double totalPrice) {
+        switch (discountRateType) {
+            case STUDENT, MILITARY, NATIONL_MERIT, ORDINARY-> {
+                totalPrice = totalPrice - (totalPrice * discountRateType.getRate());
+            }
+            default -> throw new IllegalArgumentException("유효하지 않은 타입 입니다.");
         }
         return totalPrice;
     }
