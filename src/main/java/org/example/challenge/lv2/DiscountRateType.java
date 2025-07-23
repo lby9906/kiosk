5b98906f5366cbdf2b66cc06720a0ee98ff5f9da
lby9906 <lby9906@naver.com>
2025-07-23 16:04:30 +0900

    fix: challenge lv2 DiscountRateType.java enum타입 활용 코드 구현

--- src/main/java/org/example/challenge/lv2/DiscountRateType.java
+++ src/main/java/org/example/challenge/lv2/DiscountRateType.java
@@ -1,5 +1,15 @@
 package org.example.challenge.lv2;
 
 public enum DiscountRateType {
-    STUDENT, ORDINARY, MILITARY, NATIONL_MERIT
+    STUDENT(0.03), ORDINARY(0), MILITARY(0.05), NATIONL_MERIT(0.1);
+
+    private final double rate;
+
+    DiscountRateType(double rate) {
+        this.rate = rate;
+    }
+
+    public double getRate() {
+        return rate;
+    }
 }
